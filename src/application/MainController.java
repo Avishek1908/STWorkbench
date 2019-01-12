7cb5ab4a5a2cb5b6b64e76005318d53fa08f5678
bharg <bhargavauden1998@gmail.com>
2019-01-12 15:38:49 +0530

    'Last_Review'

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -18,12 +18,16 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.jfoenix.controls.JFXButton;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -33,19 +37,25 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Rectangle2D;
@@ -71,15 +81,18 @@ public class MainController {
 	@FXML
 	private TextField addFirstName, addLastName;
 	@FXML
-	private Button add_row, import_req, import_spread,saveSpreadsheet, launch_srs;
+	private JFXButton add_row, import_req, import_spread,saveSpreadsheet, launch_srs;
 	@FXML
 	private TextArea txtArea;
 	@FXML
 	private TextField txtfield16 ;
 	@FXML
 	private Label draglabel;
+	
 	@FXML
-	HBox h_box_spreadsheet;
+	private ListView<HBox> lview;
+	@FXML
+	private Tab testcaseGen;
 	
 	@FXML
 	private TableView<SRS> tblview = new TableView<SRS>();
@@ -90,6 +103,7 @@ public class MainController {
 	ObservableList<SRS> data;
 	ObservableList<SRS> paramvals;
 	ArrayList<String> Model_List;
+	
 	
 	
 	@FXML public void initialize() throws FileNotFoundException {
@@ -123,6 +137,7 @@ public class MainController {
 		import_req.setVisible(false);
 		txtfield16.setVisible(false);
 		launch_srs.setVisible(false);
+		
 		 
         TableColumn firstNameCol = new TableColumn("Parameters");
         firstNameCol.setMinWidth(100);
@@ -254,10 +269,14 @@ public class MainController {
 			            writer.println(content);
 			            writer.close();
 			            
+			            String cont = content;
+			            cont.replace(" ", "\' '");
+			            System.out.println(cont);
 			            String pythonScriptPath = "C:\\Users\\bharg\\Documents\\STWorkbench\\SRS.py";
-			    		String[] cmd = new String[2];
+			    		String[] cmd = new String[3];
 			    		cmd[0] = "python"; // check version of installed python: python -V
 			    		cmd[1] = pythonScriptPath;
+			    		cmd[2] = content;
 			    		 
 			    		// create runtime to execute external command
 			    		Runtime rt = Runtime.getRuntime();
@@ -424,44 +443,6 @@ public class MainController {
 		}
 	}
 	
-	public void exportSheetToXlsx() throws IOException
-	{
-		
-		Workbook workbook = new XSSFWorkbook();
-        Sheet spreadsheet = workbook.createSheet("sample");
-        
-        
-        Row row = spreadsheet.createRow(0);
-        
-        
-
-        for (int j = 0; j < tv.getColumns().size(); j++) {
-            row.createCell(j).setCellValue(tv.getColumns().get(j).getText());
-        }
-
-        for (int i = 0; i < tv.getItems().size(); i++) {
-            row = spreadsheet.createRow(i + 1);
-            for (int j = 0; j < tv.getColumns().size(); j++) {
-                if(tv.getColumns().get(j).getCellData(i) != null) { 
-                    row.createCell(j).setCellValue(tv.getColumns().get(j).getCellData(i).toString()); 
-                }
-                else {
-                    row.createCell(j).setCellValue("");
-                }   
-            }
-        }
-        
-        
-        FileOutputStream fileOut = new FileOutputStream(curPath+"\\modelSave.xlsx");
-        workbook.write(fileOut);
-        fileOut.close();
-        
-        Alert alert = new Alert(AlertType.INFORMATION, "Saved the Model File to Project Location", ButtonType.OK);
-        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-        alert.show();
-        
-	}
-	
 	public void SrsSelect(ActionEvent e)
 	{
 		ap_srs.setVisible(true);
@@ -472,7 +453,9 @@ public class MainController {
 		import_spread.setVisible(false);
 		saveSpreadsheet.setVisible(true);
 		launch_srs.setVisible(true);
-		h_box_spreadsheet.setVisible(false);
+		addFirstName.setVisible(false);
+		addLastName.setVisible(false);
+		add_row.setVisible(false);
 		
 	}
 	
@@ -486,7 +469,9 @@ public class MainController {
 		import_spread.setVisible(true);
 		saveSpreadsheet.setVisible(true);
 		launch_srs.setVisible(false);
-		h_box_spreadsheet.setVisible(true);
+		addFirstName.setVisible(true);
+		addLastName.setVisible(true);
+		add_row.setVisible(true);
 	}
 	
 	public void Launch_SRS_Process(ActionEvent e)
@@ -500,10 +485,14 @@ public class MainController {
             writer.println(content);
             writer.close();
             
+            String cont = content;
+            cont.replace(" ", "\' '");
+            System.out.println(cont);
             String pythonScriptPath = "C:\\Users\\bharg\\Documents\\STWorkbench\\SRS.py";
-    		String[] cmd = new String[2];
+    		String[] cmd = new String[3];
     		cmd[0] = "python"; // check version of installed python: python -V
     		cmd[1] = pythonScriptPath;
+    		cmd[2] = content;
     		 
     		// create runtime to execute external command
     		Runtime rt = Runtime.getRuntime();
@@ -636,21 +625,22 @@ public class MainController {
 		}
 	}
 	
-	public void Import_Model(ActionEvent e) throws IOException
+	public void Import_Model(String path) throws IOException
 
 	{
-		FileChooser fileChooser = new FileChooser();
-		File file = fileChooser.showOpenDialog(null);
+		/*FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showOpenDialog(null);*/
 		
-		String str = file.getAbsolutePath().toString();
-		
+		//String str = file.getAbsolutePath().toString();
+		String str = path;
+		File xl = new File(path);
 		Model_List =new ArrayList<String>();
 		
 		if(str.endsWith("xlsx"))
 		{
 			String[] sheetrows ;
 			//File f = files.get(0);
-			draglabel.setText(file.getName().toString());
+			draglabel.setText(xl.getName().toString());
 			//InputStream f = new FileInputStream("LocalHackday.xlsx");
 			InputStream ExcelFileToRead = new FileInputStream(str);
 			XSSFWorkbook  wb = new XSSFWorkbook(ExcelFileToRead);
@@ -708,17 +698,19 @@ public class MainController {
 		}
 	}
 
-	public void setxml(ActionEvent e) throws IOException
+	public void setxml(String xlname) throws IOException
 	{
 		
-		
+		File xl = new File(xlname);
+		String xln = xl.getName();
+		String finalxl = xln.substring(0, xln.lastIndexOf('.'));
 		String stpath = "C:\\Users\\"+System.getProperty("user.name")+"\\Documents\\STWorkbench";
 		 
-	        System.out.println(curPath);
-	        System.out.println(curPath.substring(curPath.lastIndexOf("\\")+1));
+	        //System.out.println(curPath);
+	        //System.out.println(curPath.substring(curPath.lastIndexOf("\\")+1));
 	        String Slinex=curPath.substring(curPath.lastIndexOf("\\")+1);
 	        
-	        BufferedWriter writer4 = new BufferedWriter(new FileWriter(curPath+"\\"+"index.xml"));
+	        BufferedWriter writer4 = new BufferedWriter(new FileWriter(curPath+"\\"+finalxl+".xml"));
        		
 	        
 	        writer4.write ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
@@ -808,39 +800,168 @@ public class MainController {
 	}
 	
 	public void SaveSRSSpread() throws IOException
+
 	{
-		Workbook workbook = new XSSFWorkbook();
-        Sheet spreadsheet = workbook.createSheet("sample");
+		TextInputDialog dialog = new TextInputDialog("");
         
-        
-        Row row = spreadsheet.createRow(0);
-        
-        
+        dialog.setTitle("Save SRS File");
+        dialog.setHeaderText("Enter the FileName");
+        dialog.setContentText("Name:");
+ 
+        Optional<String> result = dialog.showAndWait();
+ 
+        result.ifPresent(name -> {
+        	Workbook workbook = new XSSFWorkbook();
+            Sheet spreadsheet = workbook.createSheet("sample");
+            
+            
+            Row row = spreadsheet.createRow(0);
+            
+            
 
-        for (int j = 0; j < tblview.getColumns().size(); j++) {
-            row.createCell(j).setCellValue(tblview.getColumns().get(j).getText());
-        }
-
-        for (int i = 0; i < tblview.getItems().size(); i++) {
-            row = spreadsheet.createRow(i + 1);
             for (int j = 0; j < tblview.getColumns().size(); j++) {
-                if(tblview.getColumns().get(j).getCellData(i) != null) { 
-                    row.createCell(j).setCellValue(tblview.getColumns().get(j).getCellData(i).toString()); 
-                }
-                else {
-                    row.createCell(j).setCellValue("");
-                }   
+                row.createCell(j).setCellValue(tblview.getColumns().get(j).getText());
             }
-        }
+
+            for (int i = 0; i < tblview.getItems().size(); i++) {
+                row = spreadsheet.createRow(i + 1);
+                for (int j = 0; j < tblview.getColumns().size(); j++) {
+                    if(tblview.getColumns().get(j).getCellData(i) != null) { 
+                        row.createCell(j).setCellValue(tblview.getColumns().get(j).getCellData(i).toString()); 
+                    }
+                    else {
+                        row.createCell(j).setCellValue("");
+                    }   
+                }
+            }
+            
+            
+            try {
+                FileOutputStream fileOut = new FileOutputStream(curPath+"\\"+name+".xlsx");
+                workbook.write(fileOut);
+                fileOut.close();
+                }
+                catch(Exception e)
+                {
+                	
+                }
+            
+            Alert alert = new Alert(AlertType.INFORMATION, "Saved the SRS File to Project Location", ButtonType.OK);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert.show();
+        });
+        
+		
+	}
+	
+	public void exportSheetToXlsx() throws IOException
+	{
+        TextInputDialog dialog = new TextInputDialog("");
+        
+        dialog.setTitle("Save Model File");
+        dialog.setHeaderText("Enter the FileName");
+        dialog.setContentText("Name:");
+ 
+        Optional<String> result = dialog.showAndWait();
+ 
+        result.ifPresent(name -> {
+        	
+        	Workbook workbook = new XSSFWorkbook();
+            Sheet spreadsheet = workbook.createSheet("sample");
+            
+            
+            Row row = spreadsheet.createRow(0);
+            
+            
+
+            for (int j = 0; j < tv.getColumns().size(); j++) {
+                row.createCell(j).setCellValue(tv.getColumns().get(j).getText());
+            }
+
+            for (int i = 0; i < tv.getItems().size(); i++) {
+                row = spreadsheet.createRow(i + 1);
+                for (int j = 0; j < tv.getColumns().size(); j++) {
+                    if(tv.getColumns().get(j).getCellData(i) != null) { 
+                        row.createCell(j).setCellValue(tv.getColumns().get(j).getCellData(i).toString()); 
+                    }
+                    else {
+                        row.createCell(j).setCellValue("");
+                    }   
+                }
+            }
+            
+            try {
+            FileOutputStream fileOut = new FileOutputStream(curPath+"\\"+name+".xlsx");
+            workbook.write(fileOut);
+            fileOut.close();
+            }
+            catch(Exception e)
+            {
+            	
+            }
+            
+        	Alert alert = new Alert(AlertType.INFORMATION, "Saved the Model File to Project Location", ButtonType.OK);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert.show();
+        });
         
         
-        FileOutputStream fileOut = new FileOutputStream(curPath+"\\SRSSave.xlsx");
-        workbook.write(fileOut);
-        fileOut.close();
-        
-        Alert alert = new Alert(AlertType.INFORMATION, "Saved the SRS File to Project Location", ButtonType.OK);
-        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-        alert.show();
+	}
+	
+	public void TestCaseGen(Event e)
+	{
+		File folder = new File(curPath);
+		File[] listOfFiles = folder.listFiles();
+		
+		int random = (int)(Math.random() * 50 + 1);
+		
+		ArrayList<HBox> fileNames = new ArrayList<>();
+		for (int i = 0; i < listOfFiles.length; i++) {
+		  if (listOfFiles[i].isFile()) {
+			  if(listOfFiles[i].getName().contains("xlsx")) {
+				  Label label = new Label("");
+				  label.setText(listOfFiles[i].getName());
+				  Label rand = new Label("");
+				  rand.setText(""+(int)(Math.random() * 50 + 1));
+				  HBox temp = new HBox(label, rand);
+			  fileNames.add(temp);
+			  }
+		    System.out.println("File " + listOfFiles[i].getName());
+		  } else if (listOfFiles[i].isDirectory()) {
+		    System.out.println("Directory " + listOfFiles[i].getName());
+		  }
+		}
+		
+		ObservableList<HBox> data = FXCollections.observableArrayList(fileNames);
+		//lview.getItems().addAll("Bhargav", "Avishek","Pooja");
+		lview.setItems(data);
+	}
+
+	public void OnListClick()
+	{
+		 lview.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+		        @Override
+		        public void handle(MouseEvent event) {
+		            System.out.println("clicked on " + lview.getSelectionModel().getSelectedItem());
+		            HBox click = lview.getSelectionModel().getSelectedItem();
+		            String clic = click.getChildren().get(0).toString();
+		            clic = clic.substring(clic.indexOf('\'')+1, clic.lastIndexOf('\''));
+		            System.out.println(clic);
+		            try {
+		            	String xml = clic.substring(0,clic.lastIndexOf('.'));
+						Import_Model(curPath+"\\"+clic);
+						setxml(curPath+"\\"+clic);
+						Alert alert = new Alert(AlertType.INFORMATION, "The TestCase File has been prepared for you. \n You can launch ACTS and browse to the "+xml+".xml", ButtonType.OK);
+			            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+			            alert.show();
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        }
+		    });
 	}
 	
 	public static class SRS {
